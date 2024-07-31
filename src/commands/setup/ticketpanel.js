9b1920a33cd443a1687925e2f9b540fdb1f130c0
Ray N <nyaig001@gmail.com>
2024-07-31 12:55:28 -0500

    multiple ticketpanels

--- src/commands/setup/ticketpanel.js
+++ src/commands/setup/ticketpanel.js
@@ -1,41 +1,63 @@
 const Discord = require('discord.js');
-
 const ticketSchema = require("../../database/models/tickets");
 
 module.exports = async (client, interaction, args) => {
     const name = interaction.options.getString('name');
     const description = interaction.options.getString('description');
+    const panelName = interaction.options.getString('panelName'); // New field for unique panel identifier
 
-    ticketSchema.findOne({ Guild: interaction.guild.id }, async (err, ticketData) => {
+    ticketSchema.findOne({ Guild: interaction.guild.id, PanelName: panelName }, async (err, ticketData) => {
+        if (err) {
+            return client.errNormal({
+                error: `An error occurred while fetching the ticket data.`,
+                type: 'editreply'
+            }, interaction);
+        }
+
+        const channel = interaction.guild.channels.cache.get(ticketData ? ticketData.Channel : null);
+        const button = new Discord.ButtonBuilder()
+            .setCustomId(`Bot_openticket_${panelName}`)
+            .setLabel(name)
+            .setStyle(Discord.ButtonStyle.Primary)
+            .setEmoji('🎫');
+
+        const row = new Discord.ActionRowBuilder()
+            .addComponents(button);
+
         if (ticketData) {
-            const channel = interaction.guild.channels.cache.get(ticketData.Channel);
-            const button = new Discord.ButtonBuilder()
-                .setCustomId('Bot_openticket')
-                .setLabel(name)
-                .setStyle(Discord.ButtonStyle.Primary)
-                .setEmoji('🎫')
+            // Update existing panel
+            client.embed({
+                title: name,
+                desc: description,
+                components: [row]
+            }, channel);
 
-            const row = new Discord.ActionRowBuilder()
-                .addComponents(button)
+            client.succNormal({
+                text: `Ticket panel "${panelName}" has been updated successfully!`,
+                type: 'editreply'
+            }, interaction);
+        } else {
+            // Create new panel
+            const newTicketData = new ticketSchema({
+                Guild: interaction.guild.id,
+                PanelName: panelName,
+                Channel: interaction.channel.id
+            });
+
+            await newTicketData.save();
 
             client.embed({
                 title: name,
                 desc: description,
                 components: [row]
-            }, channel)
+            }, interaction.channel);
 
             client.succNormal({
-                text: `Ticket panel has been set up successfully!`,
+                text: `Ticket panel "${panelName}" has been created successfully!`,
                 type: 'editreply'
             }, interaction);
         }
-        else {
-            client.errNormal({
-                error: `Run the ticket setup first!`,
-                type: 'editreply'
-            }, interaction);
-        }
-    })
-}
+    });
+};
 
  
--- src/commands/setup/tickets.js
+++ src/commands/setup/tickets.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-
 const ticketSchema = require("../../database/models/tickets");
 
 module.exports = async (client, interaction, args) => {
@@ -7,28 +6,29 @@ module.exports = async (client, interaction, args) => {
     const role = interaction.options.getRole('role');
     const channel = interaction.options.getChannel('channel');
     const logs = interaction.options.getChannel('logs');
+    const panelName = interaction.options.getString('panelName'); // New field for unique panel identifier
 
-    ticketSchema.findOne({ Guild: interaction.guild.id }, async (err, data) => {
+    ticketSchema.findOne({ Guild: interaction.guild.id, PanelName: panelName }, async (err, data) => {
         if (data) {
             data.Category = category.id;
             data.Role = role.id;
             data.Channel = channel.id;
             data.Logs = logs.id;
             data.save();
-        }
-        else {
+        } else {
             new ticketSchema({
                 Guild: interaction.guild.id,
+                PanelName: panelName,
                 Category: category.id,
                 Role: role.id,
                 Channel: channel.id,
                 Logs: logs.id
             }).save();
         }
-    })
+    });
 
     client.succNormal({
-        text: `Tickets has been set up successfully!`,
+        text: `Ticket panel "${panelName}" has been set up successfully!`,
         type: 'editreply'
     }, interaction);
 }
